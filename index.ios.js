556153b4992d22971e22651d01fb9ebc1fb21765
chengong <654450188@qq.com>
2018-07-29 01:41:50 +0800

    add: 添加底部的tabbar, 可以点击切换页面

--- index.ios.js
+++ index.ios.js
@@ -13,11 +13,17 @@ import {
   TabBarIOS
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
+/* 去除remote debugger时候出现的waring */
+console.ignoredYellowBox = ['Remote debugger'];
+
 export default class native443 extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      times: 0
+      times: 0,
+      selectedTab: 'video'
     };
   }
 
@@ -53,16 +59,31 @@ export default class native443 extends Component {
   render() {
     console.log('render');
     return (
-      <TabBarIOS tintColor={'#ee735c'}>
-        <TabBarIOS.Item title={ '你好' } systemIcon={ 'contacts' } badge={ 123 }>
-
-        </TabBarIOS.Item>
-        <TabBarIOS.Item title={ '我的' } systemIcon={ 'contacts' } badge={ 33 }>
-
-        </TabBarIOS.Item>
-        <TabBarIOS.Item title={ '大侠' } systemIcon={ 'downloads' } >
-
-        </TabBarIOS.Item>
+      <TabBarIOS tintColor={ '#ee735c' } unselectedItemTintColor={ '#bbb596' }>
+        <Icon.TabBarItem
+          title="视频"
+          iconName="ios-videocam"
+          selected={ this.state.selectedTab === 'video' }
+          onPress={ () => { this.setState({ selectedTab: 'video' }); } }
+        >
+          <View style={ styles.container }><Text>视频</Text></View>
+        </Icon.TabBarItem>
+        <Icon.TabBarItem
+          title="录制"
+          iconName="ios-recording"
+          selected={ this.state.selectedTab === 'recording' }
+          onPress={ () => { this.setState({ selectedTab: 'recording' }); } }
+        >
+          <View style={ styles.container }><Text>录制</Text></View>
+        </Icon.TabBarItem>
+        <Icon.TabBarItem
+          title="我的"
+          iconName="ios-more"
+          selected={ this.state.selectedTab === 'more' }
+          onPress={ () => { this.setState({ selectedTab: 'more' }); } }
+        >
+          <View style={ styles.container }><Text>我的</Text></View>
+        </Icon.TabBarItem>
       </TabBarIOS>
     );
   }
