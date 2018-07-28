c5f938cf67d9119d82c2212f5f6a085ca89e3556
chengong <654450188@qq.com>
2018-07-28 18:54:35 +0800

    add: 添加eslint, 选用popular项，standard风格

--- index.ios.js
+++ index.ios.js
@@ -15,15 +15,15 @@ import {
 export default class native443 extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
+      <View style={ styles.container }>
+        <Text style={ styles.welcome }>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
+        <Text style={ styles.instructions }>
           To get started, edit index.ios.js
         </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
+        <Text style={ styles.instructions }>
+          Press Cmd+R to reload,{ '\n' }
           Cmd+D or shake for dev menu
         </Text>
       </View>
@@ -36,18 +36,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
 
 AppRegistry.registerComponent('native443', () => native443);
